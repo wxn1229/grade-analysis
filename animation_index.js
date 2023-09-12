1207bcb2280458beca34c307e1e6b3a2e6985603
wxn1229-PC <wxn1229@gmail.com>
2023-09-13 00:02:45 +0800

    need_js

---
+++ animation_index.js
@@ -0,0 +1,17 @@
+
+
+const animation = document.querySelector('.animation')
+const slider = document.querySelector('.slider')
+const img = document.querySelector('.img')
+
+
+
+const timeLine = gsap.timeline()
+
+
+console.log(timeLine)
+o
+
+
+
+
