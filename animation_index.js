e4dd38db50fcf4433d4f75ff898eacb39f2248f8
wxn1229-vivobook <wxn1229@gmail.com>
2023-09-13 11:29:16 +0800

    animation_f

--- animation_index.js
+++ animation_index.js
@@ -6,11 +6,19 @@ const img = document.querySelector('.img')
 
 
 
-const timeLine = gsap.timeline()
+const tl = gsap.timeline({ delay: 0.2, ease: "power3" })
 
 
-console.log(timeLine)
-o
+console.log(tl)
+
+
+tl
+  .set(".img img", { objectFit: "cover" })
+  .fromTo(".img", { height: "0%" }, { duration: 0.6, height: "80%" })
+  .fromTo(".img", { width: "60vw" }, { duration: 0.6, width: "90vw" })
+  .fromTo(".slider", { xPercent: -100 }, { duration: 0.6, xPercent: 0 }, "<")
+  .fromTo(".animation", { opacity: 1 }, { duration: 1, opacity: 0 })
+  .set(".animation", { display: "none" })
 
 
 
